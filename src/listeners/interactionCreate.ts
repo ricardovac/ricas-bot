404cdf9b8b9d70a24041afbbf0fe87a0969ab5d4
ricardovac <ricardoviicius@gmail.com>
2023-03-05 09:23:03 -0300

    feat(listeners): getting commands

--- src/listeners/interactionCreate.ts
+++ src/listeners/interactionCreate.ts
@@ -1,58 +1,25 @@
-import {Client, ChatInputCommandInteraction } from "discord.js";
-import { Commands } from "../Commands";
-import {Player} from "discord-player";
+import {Events, PermissionFlagsBits} from "discord.js";
+import UMClient from "../types/common/discord";
 
-export default (client: Client): void => {
-  const player = new Player(client);
-  // @ts-ignore
-  player.on("trackStart", (queue, track) => queue.metadata.channel.send(`🎶 | Now playing **${track.title}**!`))
+export default (client: UMClient): void => {
+    client.on(Events.InteractionCreate, async (interaction) => {
+        if (!interaction.isChatInputCommand()) return;
 
-  client.on("interactionCreate", async (interaction: any) => {
-    if (interaction.isCommand() || interaction.isUserContextMenuCommand()) {
-      await handleSlashCommand(client, interaction);
-    }
+        const slashCommand = client.commands.get(interaction.commandName)
+        if (!slashCommand) return
 
-    // /play track:Despacito
-    // will play "Despacito" in the voice channel
-    if (interaction.commandName === "play") {
-      if (!interaction.member.voice.channelId) return await interaction.reply({ content: "You are not in a voice channel!", ephemeral: true });
-      if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId) return await interaction.reply({ content: "You are not in my voice channel!", ephemeral: true });
-      const query = interaction.options.getString("query");
-      const queue = player.createQueue(interaction.guild, {
-        metadata: {
-          channel: interaction.channel
+        try {
+            await slashCommand.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({
+                    content: 'There was an error while executing this command!',
+                    ephemeral: true
+                });
+            } else {
+                await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
+            }
         }
-      });
-
-      // verify vc connection
-      try {
-        if (!queue.connection) await queue.connect(interaction.member.voice.channel);
-      } catch {
-        queue.destroy();
-        return await interaction.reply({ content: "Could not join your voice channel!", ephemeral: true });
-      }
-
-      const track = await player.search(query, {
-        requestedBy: interaction.user
-      }).then(x => x.tracks[0]);
-      if (!track) return await interaction.followUp({ content: `❌ | Track **${query}** not found!` });
-
-      await queue.play(track);
-
-      return await interaction.followUp({ content: `⏱️ | Loading track **${track.title}**!` });
-    }
-  });
-};
-
-const handleSlashCommand = async (
-  client: Client,
-  interaction: ChatInputCommandInteraction,
-): Promise<void> => {
-  const slashCommand = Commands.find((c) => c.name === interaction.commandName);
-  if (!slashCommand) {
-    await interaction.followUp({content: "An error has occurred"});
-    return;
-  }
-
-  await interaction.deferReply();
-};
+    })
+}
